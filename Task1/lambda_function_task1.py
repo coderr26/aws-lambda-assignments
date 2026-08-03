94d9fde945566cd42e90c3599593afd42c83c4de
Rashmi Rajput <rashmirajput@Rashmis-MacBook-Pro.local>
2026-08-03 17:32:25 +0530

    Updated Task1 Lambda code with final 30 day cleanup logic

---
+++ Task1/lambda_function_task1.py
@@ -0,0 +1,51 @@
+import boto3
+from datetime import datetime, timedelta, timezone
+
+s3_client = boto3.client("s3")
+
+BUCKET_NAME = "YOUR_BUCKET_NAME"
+
+# Production value
+AGE_LIMIT_DAYS = 30
+
+
+def lambda_handler(event, context):
+
+    current_time = datetime.now(timezone.utc)
+
+    paginator = s3_client.get_paginator("list_objects_v2")
+
+    deleted_objects = []
+
+    for page in paginator.paginate(Bucket=BUCKET_NAME):
+
+        if "Contents" not in page:
+            continue
+
+        for obj in page["Contents"]:
+
+            object_key = obj["Key"]
+            last_modified = obj["LastModified"]
+
+            age = current_time - last_modified
+
+            if age > timedelta(days=AGE_LIMIT_DAYS):
+
+                s3_client.delete_object(
+                    Bucket=BUCKET_NAME,
+                    Key=object_key
+                )
+
+                print(f"Deleted object: {object_key}")
+
+                deleted_objects.append(object_key)
+
+
+    if not deleted_objects:
+        print("No objects older than 30 days found.")
+
+
+    return {
+        "statusCode": 200,
+        "deletedObjects": deleted_objects
+    }
